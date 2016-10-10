bede615c61a314496af5e94dcb90dbb91812c3ff
Marcio Klepacz <marcioklepacz@gmail.com>
2016-10-10 10:19:44 -0400

    Add better assertions

--- Tests/LexerTests.swift
+++ Tests/LexerTests.swift
@@ -13,19 +13,29 @@ class LexerTests: XCTestCase {
     func test_lexerForCorrectString() {
         let expectedTokens = [Token.parensOpen, Token.other("+"), Token.number(1), Token.number(2),  Token.parensClose]
         let tokens = Lexer.tokenize(string: "(+ 1 2)")
+        
         XCTAssertTrue(tokens.count == expectedTokens.count)
+        for index in 0..<expectedTokens.count {
+            XCTAssertTrue(tokens[index] == expectedTokens[index])
+        }
     }
     
     func test_lexerForCorrectLongString() {
         let expectedTokens = [Token.parensOpen, Token.other("+"), Token.number(12), Token.parensClose]
         let tokens = Lexer.tokenize(string: "(+12)")
         XCTAssertTrue(tokens.count == expectedTokens.count)
+        for index in 0..<expectedTokens.count {
+            XCTAssertTrue(tokens[index] == expectedTokens[index])
+        }
     }
     
     func test_lexerForGarbage() {
         let expectedTokens = [Token.other("d"), Token.other("e"), Token.other("f")]
         let tokens = Lexer.tokenize(string: "def")
         XCTAssertTrue(tokens.count == expectedTokens.count)
+        for index in 0..<expectedTokens.count {
+            XCTAssertTrue(tokens[index] == expectedTokens[index])
+        }
     }
     
 }
