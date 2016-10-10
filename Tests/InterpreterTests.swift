5240546b954ff2d0c4944578d74c8de1c5dff898
Marcio Klepacz <marcioklepacz@gmail.com>
2016-10-10 09:59:53 -0400

    Rename whole project to Fabienne

--- Tests/InterpreterTests.swift
+++ Tests/InterpreterTests.swift
@@ -42,21 +42,4 @@ class InterpreterTests: XCTestCase {
         XCTAssertTrue(expectedResult == result)
     }
     
-    func testResultOfLongExpression() {
-        let expectedResult = 5
-        let tree = TreeNode.init(value: Token.other("+"))
-        let left = TreeNode.init(value: Token.other("-"))
-        tree.append(child: left)
-        let right = TreeNode.init(value: Token.number(4))
-        tree.append(child: right)
-        let left1stChild = TreeNode.init(value: Token.number(2))
-        let left2ndChild = TreeNode.init(value: Token.number(1))
-        
-        left.append(child: left1stChild)
-        left.append(child: left2ndChild)
-        
-        let result = try! Interpreter.eval(tree)
-        
-        XCTAssertTrue(expectedResult == result)
-    }
 }
