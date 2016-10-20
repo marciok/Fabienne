ae306b9ba1a3d96673c7d4229ef0aebd1d6f96bf
Marcio Klepacz <marcioklepacz@gmail.com>
2016-10-20 10:03:21 -0400

    Integrate LLVM

---
+++ LLVMIRGenerator.swift
@@ -0,0 +1,23 @@
+//
+//  LLVMIRGenerator.swift
+//  Fabienne
+//
+//  Created by Marcio Klepacz on 10/19/16.
+//  Copyright © 2016 Marcio Klepacz. All rights reserved.
+//
+
+import Foundation
+//import LLVM_C
+//
+//struct CodeGenerator {
+//    
+//    let module = LLVMModuleCreateWithName("Fabiene")
+//    let int32 = LLVMInt32Type()
+//    let returnType = int32
+//    var paramTypes = UnsafeMutablePointer<LLVMTypeRef?>.allocate(capacity: 2)
+//    paramTypes.initialize(from: [int32, int32])
+//    let functionType = LLVMFunctionType(returnType, paramTypes, 2, 0)
+//    let sumFunction = LLVMAddFunction(module, "sum", functionType)
+//    
+//    LLVMDumpModule(module)
+//}
