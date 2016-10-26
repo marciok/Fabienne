0a3c43ee9788c647ae1e4dc700b6febdf4ea68df
Marcio Klepacz <marcioklepacz@gmail.com>
2016-10-26 10:53:54 -0400

    Add some confetti on REPL

--- Fabienne/Driver.swift
+++ Fabienne/Driver.swift
@@ -14,36 +14,46 @@ struct Driver {
         let ModuleName = "Fabienne"
         var ctx = Context.global()
         let mod = MCJIT(name: ModuleName)
-        print("Greeting to Fabienne REPL 🍻")
+        print("🐙  Greetings to Fabienne REPL 🍻")
+        
+        let commands = CommandLine.arguments
+        let displayAST = commands.contains("--ast")
+        let displayIR = commands.contains("--ir")
         
         var input: String?
         
-        while input != ":q" {
+        while true {
             print("> ", separator: "", terminator: "")
             input = readLine()
             guard let content = input else { continue }
+            if content == ":q" { exit(0) }
+            
             let tokens = Lexer.tokenize(string: content)
             var parser = Parser(tokens: tokens)
             
             do {
                 let ast = try parser.parse()
                 for node in ast {
+                    
+                    if displayAST { print(ast) }
+                    
                     let result = try node.codeGenerate(context: &ctx, module: mod)
                     
                     switch node {
                     case .functionNode(let fun):
                         if fun.isAnonymous {
                             print(try mod.run(function: result!))
+                            
                             continue
                         }
-                        mod.dump()
+                        
+                        if displayIR { mod.dump() }
                     }
                 }
             } catch let error {
-                print(error)
+                print("\u{001B}[0;31m ❌  \(error.localizedDescription) \u{001B}[0m")
                 //TODO: Maybe clean last instruction?
             }
-            
         }
     }
 }
