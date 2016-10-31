27aa8aa510c578b261bacf8b1710d6704a0efaa5
Marcio Klepacz <marcioklepacz@gmail.com>
2016-10-31 15:09:46 -0400

    Add falling test

--- Tests/ParserTests.swift
+++ Tests/ParserTests.swift
@@ -219,6 +219,10 @@ class ParserTests: XCTestCase {
         }
     }
     
+    func test_fail()  {
+        XCTFail()
+    }
+    
 //    func test_invalidTokensExpectingValidOperator() {
 //        let tokens: [Token] = [.number(1), .number(2)]
 //        var expectedError: Error? = nil
