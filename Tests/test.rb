df77879ae5e812af3fe2aac69984813a0830207c
Marcio Klepacz <marcioklepacz@gmail.com>
2016-11-03 11:31:19 -0400

    Add end to end tests

---
+++ Tests/test.rb
@@ -0,0 +1,39 @@
+require "test/unit"
+
+Dir.chdir("../")
+system "set -o pipefail && xcodebuild test -scheme Fabienne | xcpretty -t"  or raise "INTERNAL TESTS FAILED"
+
+result = `xcodebuild -configuration Release BUILD_DIR=$PWD/build/`
+result = `./build/Release/Fabienne ./build/Release/Fabienne --file ./Tests/input_tests.fab`
+$results = result.split(" ")
+
+class IntegrationTestsFromFile < Test::Unit::TestCase
+  def test_1
+    assert $results[0] == "36"
+  end
+
+  def test_2
+    assert $results[1] == "4"
+  end
+
+  def test_3
+    assert $results[2] == "10"
+  end
+
+  def test_4
+    assert $results[3] == "31"
+  end
+
+  def test_ext_predicated
+    assert $results[4] == "0"
+  end
+
+  def test_fibonacci
+    assert $results[5] == "55"
+  end
+
+  def test_sum
+    assert $results[6] == "18"
+  end
+end
+
